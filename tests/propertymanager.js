7a7fdbb7f89878ed513c0d98052af1f4c30d09fe
geekysrm <soumyacool2012@gmail.com>
2021-09-23 03:15:29 +0530

    update folder structure

--- tests/propertymanager.js
+++ tests/propertymanager.js
@@ -1,13 +1,13 @@
 const anchor = require('@project-serum/anchor');
 
-describe('mysolanaapp', () => {
+describe('propertymanager', () => {
 
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
   it('Is initialized!', async () => {
     // Add your test here.
-    const program = anchor.workspace.Mysolanaapp;
+    const program = anchor.workspace.Propertymanager;
     const tx = await program.rpc.initialize();
     console.log("Your transaction signature", tx);
   });
