e9044d2b2870a27cb98433451a730b0e9aa99a5d
K Sai Kishan <sonusaikishan@gmail.com>
2021-09-26 18:36:59 +0530

    Adds transfer fn and tests

--- tests/propertymanager.js
+++ tests/propertymanager.js
@@ -1,5 +1,4 @@
 const anchor = require("@project-serum/anchor");
-const assert = require("assert");
 
 const { SystemProgram } = anchor.web3;
 
@@ -41,6 +40,18 @@ describe("propertymanager", () => {
     );
     console.log("Your transaction signature", tx);
 
+    await program.rpc.register(
+      "fake-address",
+      "fake",
+      "fake@fake.com",
+      "999999999",
+      {
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+        },
+      }
+    );
+
     const account = await program.account.baseAccount.fetch(
       baseAccount.publicKey
     );
@@ -50,7 +61,7 @@ describe("propertymanager", () => {
 
   it("Add Property", async () => {
     const tx = await program.rpc.addproperty(
-      "random-id",
+      "property-id",
       "property1",
       "address...",
       "dimenstions",
@@ -73,5 +84,42 @@ describe("propertymanager", () => {
     console.log("user", account.userList[0].toString());
     console.log("property list", account.userList[0].propertyList.toString());
     console.log("property", account.propertyList[0].toString());
+  });
+
+  it("Transfer Property", async () => {
+
+    let account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
+    console.log("Before....");
+    console.log("account", account);
+    console.log("user0", account.userList[0]);
+    console.log("user1", account.userList[1]);
+    console.log("property list0", account.userList[0].propertyList);
+    console.log("property list1", account.userList[1].propertyList);
+    console.log("property", account.propertyList[0]);
+
+    const tx = await program.rpc.transfer(
+      provider.wallet.publicKey.toString(),
+      "fake-address",
+      "property-id", 
+      {
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+        },
+      }
+    );
+    console.log("Your transaction signature", tx);
+
+    account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
+    console.log("After....");
+    console.log("account", account);
+    console.log("user0", account.userList[0]);
+    console.log("user1", account.userList[1]);
+    console.log("property list0", account.userList[0].propertyList);
+    console.log("property list1", account.userList[1].propertyList);
+    console.log("property", account.propertyList[0]);
   });
 });
