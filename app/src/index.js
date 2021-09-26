20cfcd72269878b919f99af8dfcb3f2c53d0245c
geekysrm <soumyacool2012@gmail.com>
2021-09-27 02:22:21 +0530

    remove unused deps

--- app/src/index.js
+++ app/src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { StoreProvider } from "easy-peasy";
 
-import reportWebVitals from "./reportWebVitals";
-
 import App from "./App";
 import { rootStore } from "./store";
 
@@ -17,8 +15,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("property-management-app")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
