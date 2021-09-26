bc3fedb5ea9bb9b474e7d71cc3e855f97b603838
geekysrm <soumyacool2012@gmail.com>
2021-09-27 01:20:53 +0530

    fix code so that it can build

--- app/src/index.js
+++ app/src/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { StoreProvider } from 'easy-peasy';
+import React from "react";
+import ReactDOM from "react-dom";
+import { StoreProvider } from "easy-peasy";
 
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
-import App from './App';
-import { rootStore } from './Store';
+import App from "./App";
+import { rootStore } from "./Store";
 
-import './index.scss';
+import "./index.scss";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<StoreProvider store={rootStore}>
-			<App />
-		</StoreProvider>
-	</React.StrictMode>,
-	document.getElementById('property-management-app')
+  <React.StrictMode>
+    <StoreProvider store={rootStore}>
+      <App />
+    </StoreProvider>
+  </React.StrictMode>,
+  document.getElementById("property-management-app")
 );
 
 // If you want to start measuring performance in your app, pass a function
