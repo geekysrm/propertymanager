ef5a430e7908d505e91bee6bc12f78ab78465ba9
K Sai Kishan <sonusaikishan@gmail.com>
2021-10-03 22:57:46 +0530

    Marker for map (Not working till now)

--- application/src/components/CustomMap.js
+++ application/src/components/CustomMap.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import ReactMapboxGl, { Layer, Feature, Marker } from 'react-mapbox-gl';
+
+import { svg } from "./HomeIcon";
 
 const Map = ReactMapboxGl({
   accessToken:
     'pk.eyJ1IjoiZ2Vla3lzcm0iLCJhIjoiY2pqOWlyYm9wMThubjNxbzVsbWZrZDFkYSJ9.qR-h7UMZRad_rFeA-GegMQ',
 });
+
+const image = new Image();
+image.src = 'data:image/svg+xml;charset=utf-8;base64,' + btoa(svg);
+const images = ['propertyMarker', image];
+
+const layoutLayer = { 'icon-image': 'propertyMarker' };
 
 export default function CustomMap() {
   return (
@@ -16,9 +24,23 @@ export default function CustomMap() {
       }}
       center={[85.824539, 20.296059]}
     >
-      <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
-        <Feature coordinates={[85.824539, 20.296059]} />
+      {/* <Marker
+        coordinates={[85.824539, 20.296059]}
+      >
+        M
+      </Marker> */}
+      {/* <Layer type="symbol" id="marker" layout={layoutLayer} images={images}>
+        <Feature
+          coordinates={[85.824539, 20.296059]}
+        />
       </Layer>
+      <Layer type="circle" paint={{
+        'circle-radius': 1000,
+        'circle-color': '#E54E52',
+        'circle-opacity': 0.8
+      }}>
+        <Feature coordinates={[85.824539, 20.296059]} />
+      </Layer> */}
     </Map>
   );
 }
