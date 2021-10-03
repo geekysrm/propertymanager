9fb7727f9f429c34077c390e99e88357a83a192f
geekysrm <soumyacool2012@gmail.com>
2021-10-03 14:42:49 +0530

    solve css issue + remove not needed packages

--- application/src/pages/WalletConnect.js
+++ application/src/pages/WalletConnect.js
@@ -1,32 +1,37 @@
 import React, { useEffect } from 'react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { Button } from "@chakra-ui/react"
-import { ArrowForwardIcon } from '@chakra-ui/icons'
+import { Button } from '@chakra-ui/react';
+import { ArrowForwardIcon } from '@chakra-ui/icons';
+
+require('@solana/wallet-adapter-react-ui/styles.css');
 
 export default function WallectConnect() {
-
-    return (
-        <div>
-            <div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    marginTop: '100px',
-                }}
-            >
-                <WalletMultiButton />
-            </div>
-            <div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    marginTop: '100px',
-                }}
-            >
-                <Button rightIcon={<ArrowForwardIcon />} colorScheme="purple:500" variant="outline">
-                    Continue
-                </Button>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          marginTop: '100px',
+        }}
+      >
+        <WalletMultiButton />
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          marginTop: '100px',
+        }}
+      >
+        <Button
+          rightIcon={<ArrowForwardIcon />}
+          colorScheme="purple:500"
+          variant="outline"
+        >
+          Continue
+        </Button>
+      </div>
+    </div>
+  );
 }
