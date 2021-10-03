0f5ea564033f9ce2a22b93e4cb0f98877d68e950
geekysrm <soumyacool2012@gmail.com>
2021-10-04 02:23:54 +0530

    update mobile navbar of homepage

--- application/src/components/Navbar.js
+++ application/src/components/Navbar.js
@@ -3,35 +3,17 @@ import {
   chakra,
   Flex,
   Icon,
-  IconButton,
   Link,
-  useColorMode,
   useColorModeValue,
-  useDisclosure,
-  CloseButton,
   Box,
-  VStack,
-  Button,
   Image,
 } from '@chakra-ui/react';
 import { useViewportScroll } from 'framer-motion';
-import { FaMoon, FaSun, FaHeart } from 'react-icons/fa';
-import {
-  AiFillGithub,
-  AiOutlineMenu,
-  AiFillHome,
-  AiOutlineInbox,
-} from 'react-icons/ai';
-import { BsFillCameraVideoFill } from 'react-icons/bs';
+import { FaHeart } from 'react-icons/fa';
+import { AiFillGithub } from 'react-icons/ai';
 import { Link as RouterLink } from 'react-router-dom';
 
 const Navbar = () => {
-  const mobileNav = useDisclosure();
-
-  const { toggleColorMode: toggleMode } = useColorMode();
-  const text = useColorModeValue('dark', 'light');
-  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
-
   const bg = useColorModeValue('white', 'gray.800');
   const ref = React.useRef();
   const [y, setY] = React.useState(0);
@@ -47,10 +29,9 @@ const Navbar = () => {
       display={{ base: 'none', md: 'flex' }}
       alignItems="center"
       as="a"
-      aria-label="Sponsor Choc UI on Open Collective"
+      aria-label="Vote for us"
       href={''}
       target="_blank"
-      rel="noopener noreferrer"
       bg="gray.50"
       borderWidth="1px"
       borderColor="gray.200"
@@ -79,43 +60,7 @@ const Navbar = () => {
       </Box>
     </Box>
   );
-  const MobileNavContent = (
-    <VStack
-      pos="absolute"
-      top={0}
-      left={0}
-      right={0}
-      display={mobileNav.isOpen ? 'flex' : 'none'}
-      flexDirection="column"
-      p={2}
-      pb={4}
-      m={2}
-      bg={bg}
-      spacing={3}
-      rounded="sm"
-      shadow="sm"
-    >
-      <CloseButton
-        aria-label="Close menu"
-        justifySelf="self-start"
-        onClick={mobileNav.onClose}
-      />
-      <Button w="full" variant="ghost" leftIcon={<AiFillHome />}>
-        Dashboard
-      </Button>
-      <Button
-        w="full"
-        variant="solid"
-        colorScheme="brand"
-        leftIcon={<AiOutlineInbox />}
-      >
-        Inbox
-      </Button>
-      <Button w="full" variant="ghost" leftIcon={<BsFillCameraVideoFill />}>
-        Videos
-      </Button>
-    </VStack>
-  );
+
   return (
     <Box pos="relative">
       <chakra.header
@@ -157,30 +102,20 @@ const Navbar = () => {
                   _hover={{ color: 'gray.600' }}
                 />
               </Link>
-              {/* <IconButton
-                                size="md"
-                                fontSize="lg"
-                                aria-label={`Switch to ${text} mode`}
-                                variant="ghost"
-                                color="current"
-                                ml={{ base: "0", md: "3" }}
-                                onClick={toggleMode}
-                                icon={<SwitchIcon />}
-                            /> */}
               {SponsorButton}
-              <IconButton
+              <Box
+                ml="4"
                 display={{ base: 'flex', md: 'none' }}
-                aria-label="Open menu"
-                fontSize="20px"
-                color={useColorModeValue('gray.800', 'inherit')}
-                variant="ghost"
-                icon={<AiOutlineMenu />}
-                onClick={mobileNav.onOpen}
-              />
+                as="a"
+                aria-label="Vote for us"
+                href={''}
+                target="_blank"
+              >
+                <Icon as={FaHeart} w="4" h="4" color="red.500" mr="2" />
+              </Box>
             </Flex>
           </Flex>
         </Flex>
-        {MobileNavContent}
       </chakra.header>
     </Box>
   );
