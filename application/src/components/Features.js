899001431988fe8813f25f2b2a8e56fc34db4a25
K Sai Kishan <sonusaikishan@gmail.com>
2021-10-10 21:49:21 +0530

    Completed landing page

--- application/src/components/Features.js
+++ application/src/components/Features.js
@@ -1,161 +1,142 @@
-import React from 'react';
+import React from "react";
+
 import {
-  chakra,
   Box,
+  Button,
+  chakra,
   Flex,
+  SimpleGrid,
   useColorModeValue,
-  Icon,
-  Stack,
-} from '@chakra-ui/react';
+  Image
+} from "@chakra-ui/react";
 
 export default function Features() {
-  const Feature = props => {
-    return (
-      <Flex>
-        <Flex shrink={0}>
-          <Flex
+  return (
+    <>
+      <Box textAlign={{ lg: 'center' }}>
+        <chakra.p
+          mt={2}
+          fontSize={{ base: '3xl', sm: '4xl' }}
+          lineHeight="10"
+          fontWeight="extrabold"
+          color={useColorModeValue('gray.900')}
+        >
+          Introducing a secure &amp; decentralized <br /> way of managing your property records
+        </chakra.p>
+      </Box>
+      <Flex
+        p={10}
+        w="full"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box
+          shadow="xl"
+          bg={useColorModeValue("white", "gray.800")}
+          px={8}
+          py={20}
+          mx="auto"
+        >
+          <SimpleGrid
             alignItems="center"
-            justifyContent="center"
-            h={12}
-            w={12}
-            rounded="md"
-            bg={useColorModeValue('brand.500')}
-            color="white"
+            columns={{ base: 1, md: 2 }}
+            mb={24}
+            spacingY={{ base: 10, md: 32 }}
+            spacingX={{ base: 10, md: 24 }}
           >
-            <Icon
-              boxSize={6}
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              {props.icon}
-            </Icon>
-          </Flex>
-        </Flex>
-        <Box ml={4}>
-          <chakra.dt
-            fontSize="lg"
-            fontWeight="medium"
-            lineHeight="6"
-            color={useColorModeValue('gray.900')}
+            <Box>
+              <chakra.h2
+                mb={4}
+                fontSize={{ base: "2xl", md: "4xl" }}
+                fontWeight="extrabold"
+                letterSpacing="tight"
+                textAlign={{ base: "center", md: "left" }}
+                color={useColorModeValue("gray.900", "gray.400")}
+                lineHeight={{ md: "shorter" }}
+              >
+                Transperent &amp; Tamper Proof Property Records
+              </chakra.h2>
+            </Box>
+            <Box border="1px" borderColor="gray.500" p={2} borderRadius={5} >
+              <Image src="/images/feature1.jpeg" alt="feature1" height="100%" objectFit="contain" />
+            </Box>
+          </SimpleGrid>
+          <SimpleGrid
+            alignItems="center"
+            columns={{ base: 1, md: 2 }}
+            flexDirection="column-reverse"
+            mb={24}
+            spacingY={{ base: 10, md: 32 }}
+            spacingX={{ base: 10, md: 24 }}
           >
-            {props.title}
-          </chakra.dt>
-          <chakra.dd mt={2} color={useColorModeValue('gray.500', 'gray.400')}>
-            {props.children}
-          </chakra.dd>
+            <Box order={{ base: "none", md: 2 }}>
+              <chakra.h2
+                mb={4}
+                fontSize={{ base: "2xl", md: "4xl" }}
+                fontWeight="extrabold"
+                letterSpacing="tight"
+                textAlign={{ base: "center", md: "left" }}
+                color={useColorModeValue("gray.900", "gray.400")}
+                lineHeight={{ md: "shorter" }}
+              >
+                High Integrity &amp; Zero Fraud Property Transfer.
+              </chakra.h2>
+            </Box>
+            <Box border="1px" borderColor="gray.500" p={2} borderRadius={5} >
+              <Image src="/images/feature2.jpeg" alt="feature1" height="100%" objectFit="contain" />
+            </Box>
+          </SimpleGrid>
+          <SimpleGrid
+            alignItems="center"
+            columns={{ base: 1, md: 2 }}
+            mb={24}
+            spacingY={{ base: 10, md: 32 }}
+            spacingX={{ base: 10, md: 24 }}
+          >
+            <Box>
+              <chakra.h2
+                mb={4}
+                fontSize={{ base: "2xl", md: "4xl" }}
+                fontWeight="extrabold"
+                letterSpacing="tight"
+                textAlign={{ base: "center", md: "left" }}
+                color={useColorModeValue("gray.900", "gray.400")}
+                lineHeight={{ md: "shorter" }}
+              >
+                Unified Property Marketplace that Eliminates Intermediaries.
+              </chakra.h2>
+            </Box>
+            <Box border="1px" borderColor="gray.500" p={2} borderRadius={5} >
+              <Image src="/images/feature3.jpeg" alt="feature1" height="100%" objectFit="contain" />
+            </Box>
+          </SimpleGrid>
+          <SimpleGrid
+            alignItems="center"
+            columns={{ base: 1, md: 2 }}
+            flexDirection="column-reverse"
+            mb={24}
+            spacingY={{ base: 10, md: 32 }}
+            spacingX={{ base: 10, md: 24 }}
+          >
+            <Box order={{ base: "none", md: 2 }}>
+              <chakra.h2
+                mb={4}
+                fontSize={{ base: "2xl", md: "4xl" }}
+                fontWeight="extrabold"
+                letterSpacing="tight"
+                textAlign={{ base: "center", md: "left" }}
+                color={useColorModeValue("gray.900", "gray.400")}
+                lineHeight={{ md: "shorter" }}
+              >
+                Robust &amp; Fast property registration.
+              </chakra.h2>
+            </Box>
+            <Box border="1px" borderColor="gray.500" p={2} borderRadius={5} >
+              <Image src="/images/feature4.jpeg" alt="feature1" height="100%" objectFit="contain" />
+            </Box>
+          </SimpleGrid>
         </Box>
       </Flex>
-    );
-  };
-  return (
-    <Flex
-      py={10}
-      w="auto"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Box py={12} bg={useColorModeValue('white', 'gray.800')} rounded="xl">
-        <Box maxW="7xl" mx="auto" px={{ base: 4, lg: 8 }}>
-          <Box textAlign={{ lg: 'center' }}>
-            <chakra.p
-              mt={2}
-              fontSize={{ base: '3xl', sm: '4xl' }}
-              lineHeight="8"
-              fontWeight="extrabold"
-              letterSpacing="tight"
-              color={useColorModeValue('gray.900')}
-            >
-              A safe &amp; secure way to store property records
-            </chakra.p>
-            <chakra.p
-              mt={4}
-              maxW="2xl"
-              fontSize="xl"
-              mx={{ lg: 'auto' }}
-              color={useColorModeValue('gray.500', 'gray.400')}
-            >
-              Lorem ipsum dolor sit amet consect adipisicing elit. Possimus
-              magnam voluptatum cupiditate veritatis in accusamus quisquam.
-            </chakra.p>
-          </Box>
-
-          <Box mt={10}>
-            <Stack
-              spacing={{ base: 10, md: 0 }}
-              display={{ md: 'grid' }}
-              gridTemplateColumns={{ md: 'repeat(2,1fr)' }}
-              gridColumnGap={{ md: 8 }}
-              gridRowGap={{ md: 10 }}
-            >
-              <Feature
-                title="Competitive exchange rates"
-                icon={
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
-                  />
-                }
-              >
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Maiores impedit perferendis suscipit eaque, iste dolor
-                cupiditate blanditiis ratione.
-              </Feature>
-
-              <Feature
-                title=" No hidden fees"
-                icon={
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
-                  />
-                }
-              >
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Maiores impedit perferendis suscipit eaque, iste dolor
-                cupiditate blanditiis ratione.
-              </Feature>
-
-              <Feature
-                title="Transfers are instant"
-                icon={
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M13 10V3L4 14h7v7l9-11h-7z"
-                  />
-                }
-              >
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Maiores impedit perferendis suscipit eaque, iste dolor
-                cupiditate blanditiis ratione.
-              </Feature>
-
-              <Feature
-                title="Mobile notifications"
-                icon={
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
-                  />
-                }
-              >
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Maiores impedit perferendis suscipit eaque, iste dolor
-                cupiditate blanditiis ratione.
-              </Feature>
-            </Stack>
-          </Box>
-        </Box>
-      </Box>
-    </Flex>
+    </>
   );
 }
--- application/src/components/Footer.js
+++ application/src/components/Footer.js
@@ -4,7 +4,7 @@ import { Center } from "@chakra-ui/react"
 
 export default function Footer() {
     return (
-        <Center bg="purple.800" h="100px" color="white">
+        <Center h="100px" color="white">
             Handcrafted by &nbsp;<b>Team Marvellous (India 🇮🇳)</b>&nbsp; &copy; &nbsp;<b>{new Date().getFullYear()}</b>
         </Center>
     )
